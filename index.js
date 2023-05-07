3823820761ff341a57d582ee9ef1c637cb335cc4
mmpeyravi <mmpeyravi@gmail.com>
2023-05-08 02:37:35 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+ // da='';
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    ws.send('something '+data);
+    //da=data;
+  });
+
+  ws.send('something ');
+});
